accb7aaea89eddce768e54bfb8fd261789f335d5
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-01 15:37:06 -0600

    add number and function buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>x</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
+        </div>
       </div>
     );
   }
